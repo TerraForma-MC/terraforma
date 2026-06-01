8901cee5f881df1b9f1a7fa21fef3967491efbba
hastagaming <rip.hastagaming@gmail.com>
2026-06-01 18:17:48 +0700

    Create app/src/main/java/com/terraforma/app/navigation/Routes.kt

---
+++ app/src/main/java/com/terraforma/app/navigation/Routes.kt
@@ -0,0 +1,12 @@
+package com.terraforma.app.navigation
+
+object Routes {
+    const val HOME = "home"
+    const val PLAY_MINECRAFT = "play_minecraft"
+    const val RUN_SERVERS = "run_servers"
+    const val DEVELOPMENT = "development"
+    const val SESSIONS = "sessions"
+    const val TERMINAL = "terminal"
+    const val STORAGE = "storage"
+    const val SETTINGS = "settings"
+}
